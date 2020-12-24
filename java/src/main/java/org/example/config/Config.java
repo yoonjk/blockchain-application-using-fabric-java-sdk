f98c3a87fbf56f361bbce963025e8eca59babe67
yoonjk <nexweb1@gmail.com>
2020-12-23 19:13:30 -0600

    add chaincode

--- java/src/main/java/org/example/config/Config.java
+++ java/src/main/java/org/example/config/Config.java
@@ -37,14 +37,14 @@ public class Config {
 	
 	public static final String ORG1_PEER_1_URL = "grpc://localhost:7056";
 	
-	
-	public static final String ORG2_PEER_1_URL = "grpc://localhost:8056";
-	
 	public static final String CHAINCODE_ROOT_DIR = "chaincode";
 	
 	public static final String CHAINCODE_1_NAME = "fabcar";
+
+	public static final String CHAINCODE_2_NAME = "mycc";
 	
 	public static final String CHAINCODE_1_PATH = "github.com/fabcar";
+	public static final String CHAINCODE_2_PATH = "github.com/mycc";
 	
 	public static final String CHAINCODE_1_VERSION = "1";
 
